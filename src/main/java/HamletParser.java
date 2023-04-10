d68d3a2da502f8ea3ef37229efc386402a56b0a3
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-04-10 17:39:49 -0400

    wrote some tests, wrote some code

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,31 @@ public class HamletParser {
         return hamletData;
     }
 
+    public void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "Leon";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+
+        pattern = Pattern.compile("Hamlet");
+        matcher = pattern.matcher(hamletData);
+        sb = new StringBuffer();
+        replacementText = "Leon";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+    }
+
+    public int findHoratio() {
+        return 0;
+    }
+
+    public int findHamlet() {
+        return 0;
+    }
 }
